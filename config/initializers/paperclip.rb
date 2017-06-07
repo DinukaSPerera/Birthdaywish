4c1a14f1143be93bcc64a41687ef1879f2116a3b
Planvoyager <pereraid@mail.uc.edu>
2017-06-06 23:28:47 -0400

    Add paperclip location

--- config/initializers/paperclip.rb
+++ config/initializers/paperclip.rb
@@ -8,4 +8,4 @@ module Paperclip
   end
 end
 
-Paperclip::Attachment.default_options[:s3_host_name] = 's3-us-west-2.amazonaws.com'
+Paperclip::Attachment.default_options[:s3_host_name] = 's3-us-east-2.amazonaws.com'
