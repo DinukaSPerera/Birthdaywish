f9c68d24f9cfb758ea3f566c386098c8c2cf3eee
Planvoyager <pereraid@mail.uc.edu>
2017-06-07 19:32:39 -0400

    Change photo size

--- app/models/book.rb
+++ app/models/book.rb
@@ -3,6 +3,6 @@ class Book < ActiveRecord::Base
 	belongs_to :category
 	has_many :reviews
 
-	has_attached_file :book_img, :styles => { :book_index => "350x500>", :book_show => "325x475>" }, :default_url => "/images/:style/missing1.jpg"
+	has_attached_file :book_img, :styles => { :book_index => "325x400>", :book_show => "325x400>" }, :default_url => "/images/:style/missing1.jpg"
   	validates_attachment_content_type :book_img, :content_type => /\Aimage\/.*\Z/
 end
